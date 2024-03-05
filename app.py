9d5ab9c10bcc7284f922b16caa825ed056061253
mr-parle <kumarparlevinay@gmail.com>
2024-03-06 02:16:32 +0530

    Update

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ def bill_splitter():
         num_people = int(request.form['num_people'])
         people = []
         for i in range(num_people):
-            name = request.form[f'person_{i + 1}']
+            name = request.form.get(f'person_{i + 1}', '')
             people.append(name)
 
         expenses = []
         for person in people:
-            expense = float(request.form[f'expense_{person}'])
+            expense = float(request.form.get(f'expense_{person}', '0'))
             expenses.append({"person": person, "expense": expense})
 
         total_expense = sum(expense["expense"] for expense in expenses)
@@ -25,35 +25,9 @@ def bill_splitter():
             owed_amount = expense["expense"] - equal_share
             owed_amounts.append({"person": expense["person"], "owed_amount": owed_amount})
 
-        creditors = [owed for owed in owed_amounts if owed["owed_amount"] > 0]
-        debtors = [owed for owed in owed_amounts if owed["owed_amount"] < 0]
+        return render_template('results.html', owed_amounts=owed_amounts)
 
-        transactions = []
-        for debtor in debtors:
-            debtor_name = debtor["person"]
-            debtor_amount = abs(debtor["owed_amount"])
-
-            while debtor_amount > 0:
-                creditor = min(creditors, key=lambda x: x["owed_amount"])
-
-                creditor_name = creditor["person"]
-                creditor_amount = creditor["owed_amount"]
-
-                # if debtor_amount >= abs(creditor_amount):
-                #     transaction_amount = abs(creditor_amount)
-                #     debtor_amount -= transaction_amount
-                #     creditor_amount += transaction_amount
-                #     creditors.remove(creditor)
-                else:
-                    transaction_amount = debtor_amount
-                    debtor_amount = 0
-                    creditor_amount += transaction_amount
-
-                transactions.append({"from": debtor_name, "to": creditor_name, "amount": transaction_amount})
-
-        return render_template('results.html', transactions=transactions)
-
-    return render_template('index.html')
+    return render_template('index.html', num_people=5) # Set num_people to a default value
 
 if __name__ == '__main__':
     app.run(debug=True)
